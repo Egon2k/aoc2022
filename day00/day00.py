b63553f35d4a8910bccb3ab43e553e153fac8d7f
Rene Trampenau <rtrampenau@gmail.com>
2022-12-06 15:12:23 +0100

    improved template

--- day00/day00.py
+++ day00/day00.py
@@ -5,11 +5,8 @@ def part2(data):
     pass
 
 if __name__ == "__main__":
-    data = []
-
     with open('day00/data.txt') as f:
-        for line in f:
-            data.append(line.strip())
+        data = f.read().splitlines()
 
     print(part1(data))
     print(part2(data))
