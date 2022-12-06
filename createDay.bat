1a57a788c5b854b6e7b62f0b720a6ae16a577919
Rene Trampenau <rtrampenau@gmail.com>
2022-12-06 15:43:04 +0100

    added daycreator

---
+++ createDay.bat
@@ -0,0 +1,9 @@
+@echo off 
+set /p date=Enter Day (with leading 0 if smaller 10): 
+set day=day%date%
+if not exist %day% (mkdir %day%) else (exit /b 0)
+cp day00\day00.py %day%\%day%.py
+cp day00\main.cpp %day%\main.cpp
+cp day00\data.txt %day%\data.txt
+cp day00\data.txt %day%\testdata.txt
+exit /b 0
