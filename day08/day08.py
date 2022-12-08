faa59ddddb5197b87e02298825ec17bfbd5149ad
Rene Trampenau <rtrampenau@gmail.com>
2022-12-08 11:29:51 +0100

    wip day08 part2

--- day08/day08.py
+++ day08/day08.py
@@ -8,27 +8,33 @@ def pivot_data(data):
     return new_data
 
 def part1(data):
-    pivot = pivot_data(data)        # pivot data (switch cols and rows)
+    pivot = pivot_data(data)                    # pivot data (switch cols and rows)
     
-    sum = len(data) * 4 - 4         # count all edge trees
+    sum = len(data) * 4 - 4                     # count all edge trees
     
-    for row in range(1,len(data)-1):
-        for col in range(1,len(data[0])-1):
-            tree_row = data[row]
-            tree_col = pivot[col]
-            tree = data[row][col]
-            if tree > max(tree_row[:col]) or \
-               tree > max(tree_row[col+1:]) or \
-               tree > max(tree_col[:row]) or \
-               tree > max(tree_col[row+1:]): 
+    for r_idx in range(1,len(data)-1):          # ignore first tree and last tree in row since already regarded as edge
+        for c_idx in range(1,len(data[0])-1):   # ignore first tree and last tree in col since already regarded as edge
+            row = data[r_idx]
+            col = pivot[c_idx]
+            tree = data[r_idx][c_idx]
+            if tree > max(row[:c_idx]) or \
+               tree > max(row[c_idx+1:]) or \
+               tree > max(col[:r_idx]) or \
+               tree > max(col[r_idx+1:]): 
                 sum += 1 
     return sum
 
+def check_sight(r_idx, c_idx, row, col):
+    pass
+
 def part2(data):
-    ...
+    pivot = pivot_data(data)
+    for r_idx in range(len(data)):
+        for c_idx in range(len(data[0])):
+            check_sight(r_idx, c_idx, data[r_idx], pivot[c_idx])
 
 if __name__ == "__main__":
-    with open('day08/data.txt') as f:
+    with open('day08/testdata.txt') as f:
         data = f.read().splitlines()
 
     print(part1(data))
