5ee432b23a399780f5f8dd0b7fedf095bcacaa23
Rene Trampenau <rtrampenau@gmail.com>
2022-12-10 16:34:15 +0100

    solved day10

--- day10/day10.py
+++ day10/day10.py
@@ -25,11 +25,41 @@ def part1(data):
         
 
 def part2(data):
-    pass
+    grid = [['.' for _ in range(40)] for _ in range(6)]
+    cycles = 0
+    register = 1
+    
+    for line in data:
+        sprite = [register - 1, register, register +1]
+    
+        if line.startswith('add'):
+            instr, val = line.split(' ')
+            add_cycles = 2
+        else:
+            instr = 'noop'
+            add_cycles = 1
+
+        for _ in range(add_cycles):
+            col = int(cycles % 40)
+            row = int(cycles / 40)
+            cycles += 1
+            if col in sprite:
+                grid[row][col] = '#'
+
+        if instr.startswith('add'):
+            register += int(val)
+
+    return grid    
+
+def print_grid(grid):
+    for line in grid:
+        for c in line:
+            print(c, end = '')
+        print('')
 
 if __name__ == "__main__":
     with open('day10/data.txt') as f:
         data = f.read().splitlines()
 
     print(part1(data))
-    print(part2(data))
+    print_grid(part2(data))
