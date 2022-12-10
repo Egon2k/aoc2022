8ae355fffc0d66a38c5b83a64544087dc57e2eb2
Rene Trampenau <rtrampenau@gmail.com>
2022-12-10 16:08:47 +0100

    solved day10 part1

--- day10/day10.py
+++ day10/day10.py
@@ -1,21 +1,26 @@
 def part1(data):
-    cycles = 0
+    cycles = -20        # starting at -20 because 20 is the first time, an interesting signal 
+                        # appears, after that we can calculate with modulo 40
+    add_cycles = 3
     register = 1
     sum = 0
     
     for line in data:
-        cycles_old = cycles
         if line.startswith('add'):
             instr, val = line.split(' ')
-            cycles += 3
-            register += int(val)
+            add_cycles = 2
         else:
             instr = 'noop'
-            cycles += 1
+            add_cycles = 1
 
-        if cycles % 20 == 0:
-            sum += (cycles // 20) * 20 * register
-     
+        for _ in range(add_cycles):
+            cycles += 1
+            if cycles == 0 or (cycles % 40 == 0):
+                sum += ((cycles + 20) // 20) * 20 * register
+        
+        if instr.startswith('add'):
+            register += int(val)
+
     return sum
         
 
@@ -23,7 +28,7 @@ def part2(data):
     pass
 
 if __name__ == "__main__":
-    with open('day10/testdata.txt') as f:
+    with open('day10/data.txt') as f:
         data = f.read().splitlines()
 
     print(part1(data))
